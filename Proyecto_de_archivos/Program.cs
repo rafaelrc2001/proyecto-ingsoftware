eb73663728eb666963a22963a0ded59ea905fa94
Diego Ramirez <rafaelrc2001@gmail.com>
2022-07-31 15:28:41 -0500

    Añadir proyecto principal

---
+++ Proyecto_de_archivos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto_de_archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
