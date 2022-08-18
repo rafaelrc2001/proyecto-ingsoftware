4a4999ab20a5305bf5248c061267b4ec85295a2f
Diego Ramirez <rafaelrc2001@gmail.com>
2022-08-17 20:54:49 -0500

    Bump version: 0.0.4 → 0.0.5

--- Proyecto_de_archivos/Program.cs
+++ Proyecto_de_archivos/Program.cs
@@ -10,7 +10,7 @@ namespace Proyecto_de_archivos
 
     public class Program
     {
-        public string ProyectoVersion { get; } = "0.0.4";
+        public string ProyectoVersion { get; } = "0.0.5";
 
         public static int Main(string[] args)
         {
